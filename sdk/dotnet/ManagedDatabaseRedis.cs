cb2657300bbf86a48b379ffebf270a69412efb3b
Toni Kangas <kangasta@users.noreply.github.com>
2024-10-16 16:26:55 +0300

    fix: add missing `v` to version tag in `PluginDownloadURL` (#15)

--- sdk/dotnet/ManagedDatabaseRedis.cs
+++ sdk/dotnet/ManagedDatabaseRedis.cs
@@ -192,7 +192,7 @@ namespace Pulumi.Upcloud
             var defaultOptions = new CustomResourceOptions
             {
                 Version = Utilities.Version,
-                PluginDownloadURL = "https://github.com/UpCloudLtd/pulumi-upcloud/releases/download/${VERSION}/",
+                PluginDownloadURL = "https://github.com/UpCloudLtd/pulumi-upcloud/releases/download/v${VERSION}/",
                 AdditionalSecretOutputs =
                 {
                     "servicePassword",
